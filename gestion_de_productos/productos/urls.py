b4f16b9a1f4b178f1236b0c5db136b98c8e25a49
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2024-12-08 22:56:34 -0300

    feat: agregar funcionalidad de inicio y cierre de sesión con vistas y plantilla de login

--- gestion_de_productos/productos/urls.py
+++ gestion_de_productos/productos/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("registro/", views.registro, name="registro"),
     path("resultado/", views.resultado, name="resultado"),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- gestion_de_productos/productos/views.py
+++ gestion_de_productos/productos/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, redirect
 from .models import Producto
 from .forms import ProductoForm
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+
+@login_required
 def index(request):
     productos = Producto.objects.all()
     return render(request, 'templates/index.html', {'productos': productos})
 
+@staff_member_required
 def registro(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
@@ -16,6 +23,7 @@ def registro(request):
         form = ProductoForm()
     return render(request, 'templates/registro.html', {'form': form})
 
+@login_required
 def resultado(request):
     producto = None
     error = None
@@ -26,3 +34,21 @@ def resultado(request):
         except Producto.DoesNotExist:
             error = "Producto no encontrado"
     return render(request, 'templates/resultado.html', {'producto': producto, 'error': error})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'templates/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
